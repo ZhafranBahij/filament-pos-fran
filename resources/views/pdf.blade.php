b3212d7d70df93db9bd5d90778e49fc18fbe3b62
Rainfog <muhammadzhafranbahij@gmail.com>
2024-05-10 21:17:38 +0700

    Fix: Display image in pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,4 +1,4 @@
 <div>Name: {{ $record->name }}</div>
 <div>Stock: {{ $record->stock }}</div>
 <div>Price: {{ $record->price }}</div>
-{{-- <div>Image:  <img src="{{ asset('storage/'.$record->image) }}" alt=""> </div> --}}
+<div>Image:  <img src="{{ public_path('storage/'.$record->image) }}" alt=""> </div>
